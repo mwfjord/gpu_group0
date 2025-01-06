ba4a15cb00afb2696095d69b5df4ffc472711d4c
Marcus Warnerfjord <mwfjord@gmail.com>
2025-01-06 09:47:41 +0100

    more

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,8 @@
 apt -y install build-essential dkms libglfw3-dev pkg-config libglvnd-dev
+
 export OptiX_INSTALL_DIR=/OptiX/
 export OptiX_INCLUDE=/OptiX/include/
-
+export OptiX_ROOT_DIR=/OptiX/
 mkdir optix_owl_version/build
 cd optix_owl_version/build
 cmake ..
